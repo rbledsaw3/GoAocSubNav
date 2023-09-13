5d016a5c12eff8744ae04c01208de87daae32cb6
Robert Bledsaw III <rbledsaw3@outlook.com>
2023-09-13 13:55:24 -0400

    Basic error handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,21 @@
 package main
 
-type GoEnum = int
-const (
-    Foo GoEnum = iota
-    Bar
-    Baz
-)
+import "fmt"
+
+func returnsError(value int) error {
+    return fmt.Errorf("This is an error with value %v", value)
+}
+
+type Foo struct { }
+
+func (f *Foo) thisIsOnFoo() error {
+    return fmt.Errorf("This is an error from Foo")
+}
+
 
 func main(){
+    err := returnsError(5)
+    foo := Foo{}
 
+    foo.thisIsOnFoo();
 }
